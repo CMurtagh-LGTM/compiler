a7ee9c288586f965a33ded42cb3deb4ebe9bdce6
cameron <cameron.murtagh00@gmail.com>
2023-11-13 18:07:23 +1100

    fix ignoring /

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -49,7 +49,8 @@ namespace lexer {
                 }
                 else {
                     stream.unget();
-                    continue;
+                    peek = '/';
+                    break;
                 }
             }
             else {
